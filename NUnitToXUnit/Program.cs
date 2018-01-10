a777dd4446321f66b43b643b399dd23108c6d46d
Razin <Mohamedrazin.Memon@thetrainline.com>
2018-01-10 10:23:20 +0000

    Add project files.

---
+++ NUnitToXUnit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NUnitToXUnit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
